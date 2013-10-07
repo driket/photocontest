7a0dadcfe19cd6dc4627e94c2fa038253ee6ae9b
Cedric Villa <cedric.villa@gmail.com>
2013-10-07 13:37:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoContest::Application.config.secret_token = '4f27adae7934d55fbec6b3d32a98b67807656065004450d33bbaba7316dcbe85419f76859b8ab036604cff1236c9ff2c5812a3a79bf89e6c564b5a4f6753868b'
