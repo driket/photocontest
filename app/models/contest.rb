c3ddc01a56c0b4b7ba452af2dfe21b633f4605eb
Cedric Villa <cedric.villa@gmail.com>
2013-10-07 22:57:33 +0200

    add contest -> photos relationship

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,3 +1,5 @@
 class Contest < ActiveRecord::Base
   attr_accessible :body, :description, :action_title, :date_end, :date_start, :max_photos_per_user, :max_vote_per_user, :title, :vote_end, :vote_start
+  
+  has_many :photos
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :body, :title, :description, :user_uid, :contest_id
+  
+  belongs_to :contest
 end
