b30da37fb404491c066d71873273df159b442332
Cedric Villa <cedric.villa@gmail.com>
2013-10-08 15:12:15 +0200

    add paperclip image to photos + missing image PNG

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,8 @@ class Photo < ActiveRecord::Base
   attr_accessible :body, :title, :description, :user_uid, :contest_id
   
   belongs_to :contest
+  
+  attr_accessible :image
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/missing.png"
+
 end
