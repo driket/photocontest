b94055c03f8a0fd9d75d3434ddd80b9e2aac2643
Cedric Villa <cedric.villa@gmail.com>
2013-10-08 17:30:14 +0200

    add 2 function : status + vote_status

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,4 +2,24 @@ class Contest < ActiveRecord::Base
   attr_accessible :body, :description, :action_title, :date_end, :date_start, :max_photos_per_user, :max_vote_per_user, :title, :vote_end, :vote_start
   
   has_many :photos
+  
+  def status
+    if DateTime.now.to_i > date_end.to_i
+      return "finished"
+    elsif DateTime.now.to_i > date_start.to_i and DateTime.now.to_i < date_end.to_i
+      return "open"
+    elsif DateTime.now.to_i < date_start.to_i
+      return "waiting"
+    end
+  end
+  
+  def vote_status
+    if DateTime.now.to_i > vote_end.to_i
+      return "finished"
+    elsif DateTime.now.to_i > vote_start.to_i and DateTime.now.to_i < vote_end.to_i
+      return "open"
+    elsif DateTime.now.to_i < vote_start.to_i
+      return "waiting"
+    end
+  end
 end
