b28fc018f0c7f81bb62c02b68d63ace7ce6f15ba
Cedric Villa <cedric.villa@gmail.com>
2013-10-09 11:26:59 +0200

    refactor status/sentence helper

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,20 +1,20 @@
 module ContestsHelper
   
-  def status_label(contest)
-  	if contest.status == "waiting"
+  def status_label(status)
+  	if status == "waiting"
   		raw '<span class="label label-warning label-sm">En attente</span>'
-  	elsif contest.status == "open"
-  		raw '<span class="label label-danger label-sm">En cours</span>'
-  	elsif contest.status == "finished" 
-  		raw '<span class="label label-default label-sm">Fermé</span>'
-  	elsif contest.status == "vote_open" 
+  	elsif status == "open"
+  		raw '<span class="label label-danger label-sm">Participations ouvertes</span>'
+  	elsif status == "finished" 
+  		raw '<span class="label label-default label-sm">Participations terminées</span>'
+  	elsif status == "vote_open" 
   		raw '<span class="label label-danger label-sm">Votes en cours</span>'
-  	elsif contest.status == "vote_closed" 
-  		raw '<span class="label label-default label-sm">Votes fermés</span>'
+  	elsif status == "vote_closed" 
+  		raw '<span class="label label-success label-sm">Concours terminé</span>'
   	end
   end
     
-  def status_sentence(contest)
+  def contest_status_sentence(contest)
   	if contest.status == "waiting"
   		"Le concours commencera dans #{distance_of_time_in_words_to_now contest.date_start} et durera #{distance_of_time_in_words(contest.date_start,contest.date_end)}"
   	elsif contest.status == "open"
