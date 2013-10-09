6519ce0534aaebe166c8a2b7401656ed303a8d14
Cedric Villa <cedric.villa@gmail.com>
2013-10-09 11:16:34 +0200

    add photo upload (Paperclip)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131008121955) do
+ActiveRecord::Schema.define(:version => 20131008125342) do
 
   create_table "contests", :force => true do |t|
     t.string   "title"
@@ -33,8 +33,12 @@ ActiveRecord::Schema.define(:version => 20131008121955) do
     t.text     "description"
     t.string   "user_uid"
     t.integer  "contest_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sessions", :force => true do |t|
