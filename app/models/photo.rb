09618429ef605c20be818472d3ffe954b1a32aee
Cedric Villa <cedric.villa@gmail.com>
2013-10-14 09:47:08 +0200

    add hash for photo storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,11 @@ class Photo < ActiveRecord::Base
   belongs_to :contest
   
   attr_accessible :image
-  has_attached_file :image, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/assets/missing.png"
+  has_attached_file :image, 
+    :styles => { :medium => "300x300#", :thumb => "100x100#" }, 
+    :default_url => "/assets/missing.png",
+    :url => "/system/:hash.:extension",
+    :hash_secret => "longSecretString"
 
   validates :image, :attachment_presence => true
   
