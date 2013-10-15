08926e5fcd4015740279c52be2d054a41917b10e
Cedric Villa <cedric.villa@gmail.com>
2013-10-15 17:23:49 +0200

    sort on created_at

--- app/controllers/admin/contests_controller.rb
+++ app/controllers/admin/contests_controller.rb
@@ -8,7 +8,7 @@ class Admin::ContestsController < ApplicationController
   # GET /contests
   # GET /contests.json
   def index
-    @contests = Contest.order(:date_start).reverse_order
+    @contests = Contest.order(:created_at).reverse_order
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @contests }
