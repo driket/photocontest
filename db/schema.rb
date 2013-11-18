8912b14a2cbbdc178ef195a1ce6845f95aa90483
Cedric Villa <cedric.villa@gmail.com>
2013-11-18 14:57:53 +0100

    votes -> vote

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131118134707) do
+ActiveRecord::Schema.define(:version => 20131118135653) do
 
   create_table "contests", :force => true do |t|
     t.string   "title"
